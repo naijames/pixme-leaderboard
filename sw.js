162fa24d9667f4eb79c9114c634e0524f376ea3f
naijames <suthisangchan@gmail.com>
2026-05-12 02:37:27 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'pixme-v1';
+const ASSETS = ['./', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.url.includes('script.google.com')) return;
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
