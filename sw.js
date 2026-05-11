890b280570cca356f8dbce2f33a1d85e9a6b6059
naijames <suthisangchan@gmail.com>
2026-05-12 02:49:34 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE = 'pixme-v1';
-const ASSETS = ['./', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+const CACHE = 'pixme-v2';
+const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(ASSETS); }));
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then(function(keys) {
+  e.waitUntil(caches.keys().then(function(keys){
     return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
   }));
   self.clients.claim();
@@ -15,9 +15,5 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', function(e) {
   if (e.request.url.includes('script.google.com')) return;
-  e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));
 });
